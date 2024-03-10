a6eb8f9da935bbe4adb32d3a2243f6e495317ead
Yutong Zhang <zhangyt0704@outlook.com>
2024-03-09 19:34:35 -0800

    update readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from './HomePage';
+import LegoFlowerPage from './LegoFlowerPage';
+import ProfileStoryPage from './ProfileStoryPage';
+import ActivityPage from './ActivityPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/lego_flower" element={<LegoFlowerPage />} />
+        <Route path='/profile_story' element={<ProfileStoryPage />}/>
+        <Route path='/activity_intro' element={<ActivityPage />}/>
+      </Routes>
+    </Router>
   );
 }
 
